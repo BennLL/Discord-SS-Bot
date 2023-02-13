f587d671e62abf6664222e02e06e2ed9c4a23b08
BenLL <zihaoluo153@gmail.com>
2023-02-12 21:50:55 -0800

    Reply img works

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const puppeteer = require("puppeteer");
 const { Client, IntentsBitField, Message } = require("discord.js");
-require('dotenv').config();
+require("dotenv").config();
 
 const client = new Client({
   intents: [
@@ -20,12 +20,32 @@ client.on("messageCreate", (msg) => {
     return;
   }
 
-  if (msg.content === "hi") {
-    console.log(msg.content)
-    msg.reply("Hello");
+  // "-preview "
+  if (msg.content.startsWith("-preview ")) {
+    var site = msg.content.replace("-preview ", "");
+    msg.reply("going to website");
+
+    goingSite(site);
+
+    async function goingSite(link) {
+      const browser = await puppeteer.launch({ headless: false });
+      const page = await browser.newPage();
+      await page.goto(link, {
+        waitUntil: "networkidle2",
+      });
+      await page.setViewport({ width: 1920, height: 1080 });
+      await page.screenshot({ path: "ss.png", fullPage: true });
+
+      await browser.close();
+      msg.channel.send({ files: [{ attachment: 'ss.png' }] });
+
+      return;
+    }
   }
 });
+
 
 client.login(process.env.TOKEN);
 
 // node index.js to start bot
+// { files: [{ attachment: 'ss.png' }] }

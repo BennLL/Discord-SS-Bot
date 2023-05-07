dca5435bba398fcc30f230321baac2615bf7ebc0
BenLL <zihaoluo153@gmail.com>
2023-05-06 18:11:02 -0700

    Error catch fix and readme update

--- README.md
+++ README.md
@@ -1,11 +1,33 @@
-This is a bot to preview a web page in case a website is too suspicious.
+<h1>Discord SS Bot</h1>
+Made with
 
-Puppetter was utilzied to scrape elements of websites.
+![Discord.js](https://img.shields.io/badge/-Discord.js-%237289DA?logo=discord&logoColor=white)
+![Puppeteer.js](https://img.shields.io/badge/-Puppeteer.js-%234169E1?logo=puppeteer&logoColor=white)
+![JavaScript](https://img.shields.io/badge/-JavaScript-%23F7DF1E?logo=javascript&logoColor=white)
 
-command -preview "url"
-<p>To recieve a screenshot of a website back</p>
+<h3>Functions</h3>
+<p>The code utilizes Puppeteer, a headless browser automation library, to automate the process of loading a webpage, taking a screenshot, and sending it to a Discord chat. It includes error handling for page errors and navigation errors, allowing you to handle and display error messages.</p>
+<p>By executing this bot, you can capture screenshots of webpages and share them easily within your Discord community.</p>
 
 
 
-ex. -preview https://youtube.com
+<div align = "">
+
+![Example Command](https://img.shields.io/badge/-Example%20Command-grey?style=flat)
+
+"-preview https://github.com/" "-preview https://www.amazon.com/" 
+
+
+![Example Screenshots](https://img.shields.io/badge/-Example%20Screenshots-darkgrey?style=flat)
+
+<img src="https://i.imgur.com/URV1QY8.png" alt="Image" width="688" height="437.5">
+</div>
+
+
+
+
+
+
+
+
 
--- index.js
+++ index.js
@@ -24,11 +24,6 @@ client.on("messageCreate", async (msg) => {
   if (msg.content.startsWith("-preview ")) {
     var site = msg.content.replace("-preview ", "");
 
-    if(site.substring(0,4) !== "http"){
-        msg.reply("website does not exist :( \nlink must start with https:// or http://")
-        return;
-    }
-
     msg.reply("going to website...");
 
     goingSite(site);
@@ -36,20 +31,42 @@ client.on("messageCreate", async (msg) => {
     async function goingSite(link) {
       const browser = await puppeteer.launch({ headless: false });
       const page = await browser.newPage();
-      await page.goto(link, {
-        waitUntil: "networkidle2",
+
+      page.on("error", (err) => {
+        console.error(err);
+        const errorMsg = "An error occurred.";
+        msg.channel.send(errorMsg);
       });
-      await page.setViewport({ width: 1920, height: 1080 });
-      await page.screenshot({ path: "ss.png"});
-      //await page.screenshot({ path: "ss.png", fullPage: true });
 
-      await browser.close();
-      msg.channel.send({ files: [{ attachment: 'ss.png' }] });
+      page.on("pageerror", (err) => {
+        console.error(err);
+        const errorMsg = "An error occurred.";
+        msg.channel.send(errorMsg);
+      });
 
+      try {
+        await page.goto(link, {
+          waitUntil: "networkidle2",
+        });
+        await page.evaluate(
+          () => new Promise((resolve) => setTimeout(resolve, 3000))
+        );
+        await page.setViewport({ width: 1920, height: 1080 });
+        await page.screenshot({ path: "ss.png" });
+        // await page.screenshot({ path: "ss.png", fullPage: true });
+        // ^^ for large screenshots
+
+        await browser.close();
+        msg.channel.send({ files: [{ attachment: "ss.png" }] });
+      } catch (err) {
+        const errorMsg = "An error occurred.";
+        console.error(err);
+        msg.channel.send(errorMsg);
+        await browser.close();
+      }
     }
   }
 });
-
 
 client.login(process.env.TOKEN);
 
